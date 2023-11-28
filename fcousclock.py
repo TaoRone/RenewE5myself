50d76593672a72bf75d2754f545e037b74c54d6f
TaoRone <tr27@qq.com>
2023-11-29 00:35:25 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print('专注时间结束！')
+
+# 5分钟的专注时间
+countdown(5 * 60)
